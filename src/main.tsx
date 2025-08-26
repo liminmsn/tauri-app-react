6039ed5631621345ac49002b6ff445ef3f05784c
杨再涛 <1772081518@qq.com>
2025-08-27 00:32:29 +0800

    添加antdui

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { ConfigProvider, App, theme, ThemeConfig } from "antd";
+import Context from "./App";
+
+
+const defaultData: ThemeConfig = {
+  token: {
+    borderRadius: 6,
+    colorPrimary: '#1677ff',
+  },
+  components: {
+    Button: {
+      colorPrimary: '#00B96B',
+    },
+  }
+};
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider theme={defaultData}>
+      <App>
+        <Context />
+      </App>
+    </ConfigProvider>
   </React.StrictMode>,
 );
