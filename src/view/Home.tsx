e02cbe8b391aeb9cbf87173173725e63782096d7
杨再涛 <1772081518@qq.com>
2025-08-28 10:43:43 +0800

    调整

--- src/view/Home.tsx
+++ src/view/Home.tsx
@@ -3,7 +3,7 @@ import { Card } from "antd";
 function Home() {
     return <div className="home w-full pt-0 box-border" style={{ height: 'calc(100% - 26px)', paddingTop: '2px' }}>
         <Card className="h-full p-1">
-
+            213
         </Card>
     </div>
 }
