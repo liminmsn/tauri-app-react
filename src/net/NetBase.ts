1c54c9d9ce6281f67bdc4ca7b0c6c08aba8466e9
杨再涛 <1772081518@qq.com>
2025-08-29 16:10:16 +0800

    完成网络封装

---
+++ src/net/NetBase.ts
@@ -0,0 +1,22 @@
+import { ClientOptions, fetch } from '@tauri-apps/plugin-http'
+class NetBase {
+    private url: string = import.meta.env['VITE_URL'];
+    private init: (RequestInit & ClientOptions) = {};
+    constructor(src: string = '') {
+        this.url.concat(src);
+    }
+    get() {
+        this.init.method = 'GET';
+        return this;
+    }
+    post(body: BodyInit) {
+        this.init.method = 'POST';
+        this.init.body = body;
+        return this;
+    }
+    async send() {
+        return await fetch(this.url, this.init);
+    }
+}
+
+export { NetBase }
--- src/view/Home.tsx
+++ src/view/Home.tsx
@@ -1,9 +1,15 @@
-import { Card } from "antd";
+import { Button, Card } from "antd";
+import { NetBase } from "../net/NetBase";
 
+function get() {
+    new NetBase().get().send().then(res => {
+        console.log(res);
+    });
+}
 function Home() {
     return <div className="home w-full pt-0 box-border" style={{ height: 'calc(100% - 26px)', paddingTop: '2px' }}>
         <Card className="h-full p-1">
-            213
+            <Button onClick={get}>TEST</Button>
         </Card>
     </div>
 }
