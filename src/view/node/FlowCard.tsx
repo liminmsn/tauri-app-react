d586653fe68fca51dab79017a865ca316e3f4a97
杨再涛 <1772081518@qq.com>
2025-08-29 10:30:43 +0800

    去掉文本展示

--- src/view/node/FlowCard.tsx
+++ src/view/node/FlowCard.tsx
@@ -2,8 +2,7 @@ import { Node, NodeProps } from "@xyflow/react";
 import { Card, Image } from "antd";
 
 function FlowCard(prop: NodeProps<Node<{ src: string }>>) {
-    return <Card className="p-1 shadow-xl" style={{ transform: 'rotate(' + (Math.random() * 10 - 5) + 'deg)' }}>
-        {prop.data.src}
+    return <Card className="p-1 shadow-xl">
         <Image width={100} height={150} src={prop.data.src} />
     </Card>;
 }
