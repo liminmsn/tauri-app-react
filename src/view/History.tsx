1cdd9fc1baa770156e24bf722cafbbb90cdd8a2e
杨再涛 <1772081518@qq.com>
2025-09-18 18:20:47 +0800

    基础历史记录页面

--- src/view/History.tsx
+++ src/view/History.tsx
@@ -2,6 +2,7 @@ import { Card, Timeline } from "antd";
 import { JLHistory } from "../core/store/JL_LocalStorage";
 import { useEffect, useState } from "react";
 import { DetailType } from "../core/api/api_detail";
+import JLTitle from "../components/JL_Title";
 
 function History() {
     const [data, setData] = useState<DetailType[]>([]);
@@ -11,22 +12,30 @@ function History() {
         });
     }, [])
 
-    return <Card className="w-full h-full p-10">
-        <Timeline
-            items={
-                data.map(item => {
-                    return {
-                        color: 'green',
-                        children: <>
-                            <Card className="shadow-md w4/10">
-                                {item.left.desc}
-                            </Card>
-                        </>
-                    }
-                })
-            }
-        />
-    </Card>
+    return <div className="flex w-full h-full">
+        <Card className="shadow-md w-60 box-border p-2 pt-0 overflow-x-hidden overflow-y-auto">
+            <JLTitle>历史记录</JLTitle>
+            <Timeline
+                items={
+                    [...data,...data,...data,...data,...data,...data,...data].map(item => {
+                        return {
+                            color: 'var(--THEME_COLOR_BG)',
+                            children: <>
+                                <Card className="shadow-md p-2 cursor-pointer">
+                                    {new Date(item.time || 0).toLocaleDateString()}
+                                    <span>--</span>
+                                    {new Date(item.time || 0).toLocaleTimeString()}
+                                </Card>
+                            </>
+                        }
+                    })
+                }
+            />
+        </Card>
+        <Card className="shadow-md flex-1 ml-1">
+            123
+        </Card>
+    </div>
 }
 
 export default History;
