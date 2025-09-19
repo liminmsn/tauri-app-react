d2c234e59426434b87aa3e94a1cf60653128ce94
杨再涛 <1772081518@qq.com>
2025-09-19 12:06:49 +0800

    基础历史页面

--- src/components/Titlebar/TitleBar.tsx
+++ src/components/Titlebar/TitleBar.tsx
@@ -1,11 +1,12 @@
 import { Button, Card, Popover } from "antd";
-import { Bolt, RefreshCw, ArrowLeft, ClipboardClock } from "lucide-react";
+import { Bolt, RefreshCw, ArrowLeft, ClipboardClock, History, Undo2, CircleArrowLeft, CircleChevronLeft, RefreshCcw } from "lucide-react";
 import SettingView from "./children/SettingView";
 import ControlBtn from "./children/ControlBtn";
 import { CSSProperties, useEffect } from "react";
 import SearchIpt from "./children/SearchIpt";
 import { useNavigate } from "react-router-dom";
 import { JLHistory } from "../../core/store/JL_LocalStorage";
+import { HistoryOutlined } from "@ant-design/icons";
 
 const sys_bg: CSSProperties = { background: 'var(--THEME_COLOR)', color: "white", marginLeft: '2px' }
 
@@ -21,19 +22,19 @@ export function TitleBar() {
             <span className="ml-1 font_one select-none cursor-pointer" onClick={() => { history.go(-(history.length - 1)) }}>{import.meta.env['VITE_NAME']}</span>
         </Card>
         <Card className="mr-1 w-full app-region"></Card>
-        <Button className="mr-1 !mb-0" type={"primary"} onClick={() => navigate('/history')}>
-            <ClipboardClock size={14} />
-        </Button>
         <Button className="mr-1 !mb-0" type={"primary"} onClick={() => navigate(-1)}>
-            <ArrowLeft size={14} />
+            <CircleChevronLeft size={15} />
         </Button>
-        <Button className="mr-1 !mb-0" type={"primary"} onClick={() => globalThis.location.reload()}>
+        <Button className="mr-1 !mb-0" type={"primary"} onClick={() => navigate('/history')}>
+            <History size={15} />
+        </Button>
+        {/* <Button className="mr-1 !mb-0" type={"primary"} onClick={() => globalThis.location.reload()}>
             <RefreshCw size={14} />
-        </Button>
+        </Button> */}
         <SearchIpt />
         <Popover content={<SettingView />} trigger={"click"} placement={"bottom"}>
             <Button className="mr-1 !mb-0" type={"primary"}>
-                <Bolt size={14} />
+                <Bolt size={15} />
             </Button>
         </Popover>
         <ControlBtn />
--- src/view/History.tsx
+++ src/view/History.tsx
@@ -1,30 +1,77 @@
-import { Card, Timeline } from "antd";
+import { Button, Card, Image, Segmented, Timeline } from "antd";
 import { JLHistory } from "../core/store/JL_LocalStorage";
 import { useEffect, useState } from "react";
 import { DetailType } from "../core/api/api_detail";
 import JLTitle from "../components/JL_Title";
+import { CirclePlay, Heart, Play } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function History() {
     const [data, setData] = useState<DetailType[]>([]);
-    useEffect(() => () => {
-        new JLHistory((obj) => {
-            obj.getAll(data => setData(data));
+    const [select, setSelect] = useState<DetailType>();
+    function getSelectItem(history_item: string) {
+        let val = '';
+        data.forEach(item => {
+            item.right.volumes.forEach(item_ => {
+                item_.list.forEach(item__ => {
+                    if (item__.href == history_item) {
+                        val = item__.title;
+                    }
+                });
+            });
         });
-    }, [])
+        return val;
+    }
 
+    function fetchData() {
+        new JLHistory((obj) => {
+            obj.getAll(data => {
+                setData(data);
+                setSelect({ ...data[0] });
+            });
+        });
+    }
+
+    useEffect(() => fetchData(), []);
+    const navigate = useNavigate();
+
+    const selectStyle: React.CSSProperties = { backgroundColor: 'var(--THEME_COLOR)', color: 'white', transition: 'all 250ms ease' };
+    const selectCentext: React.CSSProperties = {
+        background: `
+        linear-gradient(to top right, rgba(0,0,0,1), transparent),
+        url('${select ? select.left.img : ''}') center`
+    };
     return <div className="flex w-full h-full">
-        <Card className="shadow-md w-60 box-border p-2 pt-0 overflow-x-hidden overflow-y-auto">
+        <Card className="shadow-md w5/10 p-2 pt-0 overflow-x-hidden overflow-y-auto">
             <JLTitle>历史记录</JLTitle>
+            <Segmented<string>
+                options={['3天', '7天', '时间不限']}
+                onChange={(value) => {
+                    console.log(value);
+                }}
+            />
             <Timeline
+                className="box-border"
+                mode={"alternate"}
                 items={
-                    [...data,...data,...data,...data,...data,...data,...data].map(item => {
+                    [...data].reverse().map(item => {
                         return {
-                            color: 'var(--THEME_COLOR_BG)',
+                            color: 'blue',
                             children: <>
-                                <Card className="shadow-md p-2 cursor-pointer">
+                                <div style={{ color: 'var(--THEME_COLOR)' }}>
                                     {new Date(item.time || 0).toLocaleDateString()}
-                                    <span>--</span>
+                                    <span>-</span>
                                     {new Date(item.time || 0).toLocaleTimeString()}
+                                </div>
+                                <span className="text-bluegray">{getSelectItem(item.history_item || '')}</span>
+                                <Card
+                                    style={item.history_item == select?.history_item ? selectStyle : undefined}
+                                    className="shadow-md p-2 cursor-pointer flex flex-col"
+                                    onClick={() => setSelect(item)}>
+                                    <div className="flex">
+                                        <Card className="w-100 effect_hover_bg_size" style={{ backgroundImage: `url('${item.left.img}')` }}></Card>
+                                        <div className="text-3 ml-2">{item.right.title.one}</div>
+                                    </div>
                                 </Card>
                             </>
                         }
@@ -32,10 +79,25 @@ function History() {
                 }
             />
         </Card>
-        <Card className="shadow-md flex-1 ml-1">
-            123
-        </Card>
-    </div>
+        <Card className="shadow-md flex-1 ml-1 overflow-hidden">
+            {
+                select ?
+                    <div className="w-full h-full flex flex-col justify-center items-center text-white" style={selectCentext}>
+                        <Card className="w-60 h-80 effect_hover_bg_size shadow-md" style={{ backgroundImage: `url('${select.left.img}')` }}></Card>
+                        <div className="w6/10 text-4 font-bold line-height-none mt-4 font_one">{select.right.title.one}</div>
+                        <div>{select.right.title.two}</div>
+                        <div className="mt-2 font-bold">{getSelectItem(select.history_item || '')}</div>
+                        <div className="my-1 w4/10 flex gap-1">
+                            <Button className="w-full" type={"primary"} icon={<CirclePlay size={14} />} onClick={() => navigate(`/video?id=${globalThis.btoa(select.history_item!)}&title=${getSelectItem(select.history_item!)}`)}>继续播放</Button>
+                            {/* <Button className="w-full" type={"primary"} icon={<Heart size={14} />}>收藏</Button> */}
+                        </div>
+                    </div> :
+                    <div className="h-full flex items-center justify-center">
+                        <Image width={80} src="/icon_2.png" preview={false} />
+                    </div>
+            }
+        </Card >
+    </div >
 }
 
 export default History;
