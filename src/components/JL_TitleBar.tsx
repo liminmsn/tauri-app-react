49fdd33ae910d2740f36642db8dc0af9a02843c5
杨再涛 <1772081518@qq.com>
2025-09-29 01:24:43 +0800

    修复返回首页按钮在首页还可以反复点击

--- src/components/JL_TitleBar.tsx
+++ src/components/JL_TitleBar.tsx
@@ -35,7 +35,11 @@ function JLTitleBar() {
         <Card className="mr-1 w-full app-region"></Card>
         <Card className="mr-1 w-2/10 app-region"></Card>
         {/* <DeviceID className="w-full" /> */}
-        <Button className="mr-1 !mb-0" type={"primary"} onClick={() => navigate(history.state.idx * -1)}>
+        <Button className="mr-1 !mb-0" type={"primary"} onClick={() => {
+            if (location.pathname != '/') {
+                navigate(history.state.idx * -1)
+            }
+        }}>
             <HousePlug size={icon_size} strokeWidth={icon_width} />
         </Button>
         <Button className="mr-1 !mb-0" type={"primary"} onClick={() => navigate(-1)}>
