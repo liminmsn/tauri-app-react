0a51a8e243855e0de9d0707d46d5a354ef7b06f2
杨再涛 <1772081518@qq.com>
2025-09-30 17:52:41 +0800

    封装公告

--- src/notify/Notify.tsx
+++ src/notify/Notify.tsx
@@ -1,27 +1,46 @@
-import { Button, Card } from "antd";
+import { Button, Card, Carousel, Image } from "antd";
 import { X } from "lucide-react";
 import React from "react";
 import icon from '../assets/icon.png';
 
+type NotifyType = {
+    show_notify: boolean;
+    notify_data: any[];
+}
+
 const NotifyStyle: React.CSSProperties = {
     position: 'fixed',
-    zIndex: '16',
     left: 0,
     top: 0,
-    background: 'rgba(0,0,0,0.01)',
+    textAlign: 'center',
+    zIndex: '16',
+    background: 'rgba(0,0,0,1)',
     backdropFilter: 'blur(0.5px)',
 };
-const Title = <div className="flex items-center">
-    <img className="app-region mr-1 h-6" srcSet={icon} />
-    <span>重要通知</span>
-</div>;
-const Close = <Button onClick={() => { }} danger type={"primary"}><X size={14} /></Button>;
+
+
+//Notify
 class Notify extends React.Component {
-    state: Readonly<{}> = {};
+    state: Readonly<NotifyType> = {
+        show_notify: true,
+        notify_data: []
+    };
+    componentDidMount() {
+
+    }
+
+    private Title = <div className="flex items-center">
+        <Image width={25} src={icon} />
+        <span className="ml-1">特别公告</span>
+    </div>;
+    private Close = <Button onClick={() => this.setState({ show_notify: false })} danger type={"primary"}><X size={14} /></Button>;
     render(): React.ReactNode {
-        return <div className="w-full h-full flex justify-center items-center" style={NotifyStyle}>
-            <Card className="w5/10 h4/10" title={Title} extra={Close}>
-                hello
+        return this.state.show_notify && <div className="w-full h-full" style={NotifyStyle}>
+            <Card className="w-100 h-auto overflow-hidden mt-30" style={{ marginInline: 'auto' }} title={this.Title} extra={this.Close}>
+                <Carousel className="h-60" arrows infinite={false}>
+                    <div className="h-60 flex">
+                    </div>
+                </Carousel>
             </Card>
         </div>
     }
